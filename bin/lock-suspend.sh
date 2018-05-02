ed052b8e7f307adbdd432019c96237a39d8b3bc7
Hendrik Schröter <h.schroeter@pm.me>
2018-05-02 08:25:16 +0200

    Fix lockscreen and suspend

--- bin/lock-suspend.sh
+++ bin/lock-suspend.sh
@@ -16,4 +16,5 @@ swaylock \
   --keyhlcolor b8bb26 --ringcolor 98971a \
   --ringvercolor 458588 --insidevercolor 83a598 \
   --ringwrongcolor cc241d --insidewrongcolor fb4934 \
-  -i /home/$USER/.cache/lockscreen.png
+  -i /home/$USER/.cache/lockscreen.png \
+  & systemctl suspend
