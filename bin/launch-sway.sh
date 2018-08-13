50ef1d5c41fa5eb91cef0a249c82aa2d38ad768b
Hendrik Schröter <hendrik.m.schroeter@fau.de>
2018-08-13 08:33:57 +0200

    Cleanup launch-sway

--- bin/launch-sway.sh
+++ bin/launch-sway.sh
@@ -3,9 +3,6 @@
 # start user dbus session
 systemctl --user start dbus
 
-#eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
-#export SSH_AUTH_SOCK
-
 # set keyboard layout (de)
 export XKB_DEFAULT_LAYOUT=de
 export XKB_DEFAULT_VARIANT=nodeadkeys
