059892847b48846f417d6bd286cdbea5de1dd714
Hendrik Schröter <hendrik.m.schroeter@fau.de>
2020-11-30 16:40:19 +0100

    bashrc: Add pyenv init script

---
+++ bashrc.d/90_pyenv.sh
@@ -0,0 +1,22 @@
+#!bash
+
+# Setup Virtualenvwrapper
+export VIRTUALENV_PYTHON=python3
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Projects
+if [ -x "$(command -v virtualenvwrapper.sh)" ]; then
+  source virtualenvwrapper_lazy.sh
+fi
+
+# Setup miniconda if found; this isn't done lazily, so expect some bash starup delay
+__conda_setup="$('$HOME/miniconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/miniconda/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/miniconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
