9994eb40af9172d3458a2abd7b6c56041e5b007e
Hendrik Schröter <hendrik.m.schroeter@fau.de>
2021-03-04 07:39:53 +0100

    fzf: Add keybindings

--- fzf.bash
+++ fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
+if [[ ! "$PATH" == */home/hendrik/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/hendrik/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/hendrik/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.bash"
+source "/home/hendrik/.fzf/shell/key-bindings.bash"
