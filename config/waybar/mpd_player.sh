bc1deaa05725c1dcb8defefd67445fb7602f5452
Hendrik Schroeter <h.schroeter@pm.me>
2021-09-24 11:35:50 +0200

    waybar: Some styling

--- config/waybar/mpd_player.sh
+++ config/waybar/mpd_player.sh
@@ -4,4 +4,9 @@ if [ "$#" -ne 1 ]; then
     echo "Usage $0 <player_name>"
 fi
 
-playerctl -F --player="$1" metadata --format '{"text": "{{title}}", "alt": "{{status}}", "class": "{{status}}", "tooltip": "{{playerName}}: {{markup_escape(title)}}"}'
+if [ $1 == "spotify" ]; then
+    # Include artist in text
+    playerctl -F --player="$1" metadata --format '{"text": "{{artist}} - {{title}}", "alt": "{{status}}", "class": "{{status}}", "tooltip": "{{playerName}}: {{markup_escape(artist)}} - {{markup_escape(title)}}"}'
+else
+    playerctl -F --player="$1" metadata --format '{"text": "{{title}}", "alt": "{{status}}", "class": "{{status}}", "tooltip": "{{playerName}}: {{markup_escape(artist)}} - {{markup_escape(title)}}"}'
+fi
