e184a3f9351983e49f6b8acea75ce132ea655a4d
Hendrik Schroeter <h.schroeter@pm.me>
2021-11-16 09:21:47 +0100

    fzf: Home folder agnostic

--- fzf.bash
+++ fzf.bash
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/hendrik/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/hendrik/.fzf/bin"
+if [[ ! "$PATH" == *"$HOME"/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/hendrik/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/hendrik/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
