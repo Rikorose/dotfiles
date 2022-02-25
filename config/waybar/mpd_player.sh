1daf42b9b770710a0aaa65ba9e1f01591fac1486
Hendrik Schroeter <h.schroeter@pm.me>
2022-02-25 10:20:52 +0100

    waybar: Add spotifyd

--- config/waybar/mpd_player.sh
+++ config/waybar/mpd_player.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage $0 <player_name>"
+if [[ "$#" -ne 1 ]]; then
+  echo "Usage $0 <player_name>"
 fi
 
-if [ $1 == "spotify" ]; then
-    # Include artist in text
-    playerctl -F --player="$1" metadata --format '{"text": "{{artist}} - {{title}}", "alt": "{{status}}", "class": "{{status}}", "tooltip": "{{playerName}}: {{markup_escape(artist)}} - {{markup_escape(title)}}"}'
+if [[ "$1" == *"spotify"* ]]; then
+  # Include artist in text
+  playerctl -F --player="$1" metadata --format '{"text": "{{artist}} - {{title}}", "alt": "{{status}}", "class": "{{status}}", "tooltip": "{{playerName}}: {{markup_escape(artist)}} - {{markup_escape(title)}}"}'
 else
-    playerctl -F --player="$1" metadata --format '{"text": "{{title}}", "alt": "{{status}}", "class": "{{status}}", "tooltip": "{{playerName}}: {{markup_escape(artist)}} - {{markup_escape(title)}}"}'
+  playerctl -F --player="$1" metadata --format '{"text": "{{title}}", "alt": "{{status}}", "class": "{{status}}", "tooltip": "{{playerName}}: {{markup_escape(artist)}} - {{markup_escape(title)}}"}'
 fi
