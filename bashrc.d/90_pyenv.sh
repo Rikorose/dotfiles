5836493a0f4b18621f82fe97b4c297dc07e7ee61
Hendrik Schroeter <h.schroeter@pm.me>
2022-07-29 13:39:16 +0200

    bashrc: Switch to mambaforge

--- bashrc.d/90_pyenv.sh
+++ bashrc.d/90_pyenv.sh
@@ -8,15 +8,15 @@ if [ -x "$(command -v virtualenvwrapper.sh)" ]; then
   source virtualenvwrapper_lazy.sh
 fi
 
-# Setup miniconda if found; this isn't done lazily, so expect some bash starup delay
-__conda_setup=$("$HOME/miniconda/bin/conda" 'shell.bash' 'hook' 2>/dev/null)
+# Setup mambaforge if found; this isn't done lazily, so expect some bash starup delay
+__conda_setup=$("$HOME/mambaforge/bin/conda" 'shell.bash' 'hook' 2>/dev/null)
 if [ $? -eq 0 ]; then
   eval "$__conda_setup"
 else
-  if [ -f "$HOME/miniconda/etc/profile.d/conda.sh" ]; then
-    . "$HOME/miniconda/etc/profile.d/conda.sh"
+  if [ -f "$HOME/mambaforge/etc/profile.d/conda.sh" ]; then
+    . "$HOME/mambaforge/etc/profile.d/conda.sh"
   else
-    export PATH="$HOME/miniconda/bin:$PATH"
+    export PATH="$HOME/mambaforge/bin:$PATH"
   fi
 fi
 unset __conda_setup
