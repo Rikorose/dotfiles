fb1cf1df39141607a98ee7d5af24cc8e99c27c3e
Hendrik Schroeter <h.schroeter@pm.me>
2022-07-29 13:37:47 +0200

    bashrc: Add ~/.local/lib to LD_LIBRARY_PATH

--- bashrc.d/00_global.sh
+++ bashrc.d/00_global.sh
@@ -3,6 +3,8 @@
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/node_modules/.bin:$PATH"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib64/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib/
 
 if [[ -f /etc/bashrc ]]; then
   . /etc/bashrc
