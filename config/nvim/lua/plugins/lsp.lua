aef9fcf839f83bc6ca78c0cf60b5c53f7134a5fb
Hendrik Schroeter <h.schroeter@pm.me>
2022-11-22 21:06:58 +0100

    nvim: Fix texlab config; fmt lsp config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,22 +1,30 @@
 local lsp = require "lsp-zero"
-local log = require("plugins.log")
 require "plugins.snips" -- load custom snippets
 
-vim.diagnostic.config({
+vim.diagnostic.config {
   underline = {
     severity = { min = vim.diagnostic.severity.ERROR },
   },
-})
+}
 
-lsp.preset("recommended")
-lsp.set_preferences({
+-- LSP zero setup
+lsp.ensure_installed {
+  "texlab",
+}
+lsp.preset "recommended"
+lsp.set_preferences {
   set_lsp_keymaps = false,
-})
-lsp.on_attach(function(client, bufnr)
-  log.info("Attaching language server", client.name)
+}
+lsp.on_attach(function(_, bufnr)
   vim.api.nvim_create_autocmd("CursorHold", {
     buffer = bufnr,
     callback = function()
+      -- Only open diagnostic float if no other floating windows are open
+      for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if vim.api.nvim_win_get_config(winid).zindex then
+          return
+        end
+      end
       local opts = {
         focusable = false,
         close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
@@ -37,55 +45,67 @@ lsp.on_attach(function(client, bufnr)
   bind("n", "gt", vim.lsp.buf.type_definition, noremap)
   bind("n", "<ctrl-k>", vim.lsp.buf.signature_help, noremap)
 end)
-require("rust-tools").setup({
-  tools = {
-    autoSetHints = true,
-    inlay_hints = {
-      only_current_line = true,
+lsp.configure("texlab", {
+  settings = {
+    texlab = {
+      chktex = { onEdit = true },
+      formatterLineLength = 120,
+      latexFormatter = "latexindent",
+      forwardSearch = {
+        args = { "--synctex-forward", "%l:1:%f", "%p" },
+        executable = "zathura",
+      },
     },
   },
-  server = lsp.build_options("rust_analyzer", {}),
-})
-local null_ls = require("null-ls")
-local null_opts = lsp.build_options("null-ls", {})
-null_ls.setup({
-  on_attach = null_opts.on_attach,
-  sources = {
-    -- null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.fixjson.with({ filetypes = { "jsonc", "json" } }),
-    null_ls.builtins.formatting.isort,
-    null_ls.builtins.formatting.black.with({ args = { "--quiet", "-" } }),
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.formatting.shfmt,
-    null_ls.builtins.diagnostics.markdownlint,
-  },
 })
 
+lsp.nvim_workspace()
 lsp.setup()
 
+-- Null ls
+local null_ls = require "null-ls"
+local null_opts = lsp.build_options("null-ls", {})
+null_ls.setup {
+  on_attach = null_opts.on_attach,
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.fixjson.with { filetypes = { "jsonc", "json" } },
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black.with { args = { "--quiet", "-" } },
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.formatting.shfmt,
+    null_ls.builtins.formatting.yamlfmt,
+    null_ls.builtins.diagnostics.markdownlint,
+  },
+}
+
+-- Show signatures in insert mode
 require("lsp_signature").setup()
-require("trouble").setup({
+
+-- Diagnostics in extra split
+require("trouble").setup {
   auto_open = false,
   use_diagnostic_signs = true,
-})
-require("lsp-colors").setup({
+}
+require("lsp-colors").setup {
   Error = "#db4b4b",
   Warning = "#e0af68",
   Information = "#0db9d7",
   Hint = "#10B981",
-})
+}
 
+-- Formatting
 vim.keymap.set("n", "<F5>", function()
-  vim.lsp.buf.format({
+  vim.lsp.buf.format {
     async = true,
     filter = function(client)
       if client.name == "sumneko_lua" then
         return false
       end
-      log.info("trying to format with " .. client.name)
       return true
     end,
-  })
+  }
 end)
 
 vim.keymap.set("n", "<leader>cs", "<cmd>WorkspaceSymbols<cr>")
